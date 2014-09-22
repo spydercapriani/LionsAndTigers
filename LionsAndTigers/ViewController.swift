db8f38237c5a98837b4dcf8239ac4316e00113c1
Danny Gilbert <spydercapriani@gmail.com>
2014-09-22 11:36:14 -0500

    Second Commit
    Corrected bad use of universal variables for the array & index
    
    Added functionality:
    + Random button = Allows user to cycle through tigers array randomly

--- LionsAndTigers/ViewController.swift
+++ LionsAndTigers/ViewController.swift
@@ -8,28 +8,27 @@
 
 import UIKit
 
-let firstTiger = Tiger(name: "Tigger", age: 3, breed: "Bengal", image: UIImage(named:"BengalTiger.jpg"))
-let secondTiger = Tiger(name: "Tigress", age: 8, breed: "Indochinese", image: UIImage(named:"IndochineseTiger.jpg"))
-let thirdTiger = Tiger(name: "Jacob", age: 5, breed: "Malayan", image: UIImage(named:"MalayanTiger.jpg"))
-let fourthTiger = Tiger(name: "Spar", age: 9, breed: "Siberian", image: UIImage(named:"SiberianTiger.jpg"))
-let myTigers:[Tiger] = [firstTiger,secondTiger,thirdTiger,fourthTiger]
-
-var index:Int = 0
 
 class ViewController: UIViewController {
     @IBOutlet weak var myImageView: UIImageView!
     @IBOutlet weak var lblName: UILabel!
     @IBOutlet weak var lblAge: UILabel!
     @IBOutlet weak var lblBreed: UILabel!
+    
+    var myTigers:[Tiger] = []
+    var index:Int = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        myImageView.image = myTigers[0].image
-        lblName.text = myTigers[0].name
-        lblBreed.text = myTigers[0].breed
-        lblAge.text = "\(myTigers[0].age)"
+        let firstTiger = Tiger(name: "Tigger", age: 3, breed: "Bengal", image: UIImage(named:"BengalTiger.jpg"))
+        let secondTiger = Tiger(name: "Tigress", age: 8, breed: "Indochinese", image: UIImage(named:"IndochineseTiger.jpg"))
+        let thirdTiger = Tiger(name: "Jacob", age: 5, breed: "Malayan", image: UIImage(named:"MalayanTiger.jpg"))
+        let fourthTiger = Tiger(name: "Spar", age: 9, breed: "Siberian", image: UIImage(named:"SiberianTiger.jpg"))
+        myTigers += [firstTiger, secondTiger, thirdTiger, fourthTiger]
+        
+        displayTiger()
     }
 
     override func didReceiveMemoryWarning() {
@@ -37,26 +36,36 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-    @IBAction func btnBarNextPressed(sender: UIBarButtonItem) {
+    @IBAction func btnBarNextPressed(sender: UIBarButtonItem) { // cycle forwards through tigers
         if (myTigers.count - 1) == index {
             index = 0
         }else {
             index++
         }
-        
-        myImageView.image = myTigers[index].image
-        lblName.text = myTigers[index].name
-        lblBreed.text = myTigers[index].breed
-        lblAge.text = "\(myTigers[index].age)"
+        displayTiger()
     }
 
-    @IBAction func btnBarPrevPressed(sender: UIBarButtonItem) {
+    @IBAction func btnBarPrevPressed(sender: UIBarButtonItem) { // cycle backwards through tigers
         if 0 == index {
             index = myTigers.count - 1
         }else {
             index--
         }
+        displayTiger()
+    }
+    
+    @IBAction func btnRandomPressed(sender: UIBarButtonItem) { // cycle randomly through tigers
+        var randomIndex = Int(arc4random_uniform(UInt32(myTigers.count)))
         
+        while randomIndex == index { // ensures different tiger is chosen each time random button is pressed
+            randomIndex = Int(arc4random_uniform(UInt32(myTigers.count)))
+        }
+        index = randomIndex
+        
+        displayTiger()
+    }
+    
+    func displayTiger(){
         myImageView.image = myTigers[index].image
         lblName.text = myTigers[index].name
         lblBreed.text = myTigers[index].breed
